72a83860ab826b71ddda2b68931e676f6b08431c
mirthepeer <mirthepeer@gmail.com>
2022-08-20 04:26:31 -0400

    UI Redesigned using Material-UI

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,25 +1,52 @@
 import React from 'react'
 import './Nav.css'
+import { AppBar, Button, } from '@material-ui/core'
+import Topography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid';
+import { Icon } from "@material-ui/core";
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 
 const Nav = () => {
   return (
-    <div className='Nav-bar'>
-        <div className='title'>
-            <h1>Random Quote Generator</h1>
-        </div>
+   
+    <Grid
+    container
+   style={{ minHeight: '15vh' }}>
 
-    <div className='nav-element'>
-    <a href='https://github.com/mirthepeer/Random_QuoteAPI'>
-        <div className='view-link'>
-            <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVrnjjT5fU58JiJk9w4L6VXCNtVxajn3Ygt3mWSGodgQ&s'></img></div>
-            <div><p className='view-code'>  View Code </p></div>
-        </div>
+
+
+
+   <AppBar style={{alignItems: 'center'}}>
+    <Topography variant="h4">Random Quote Generator</Topography>
+    <Button
+    size='large'
+    endIcon={<GitHubIcon/>}
+    href='https://github.com/mirthepeer/Random_QuoteAPI'
+    >View Code</Button>
+    
+   </AppBar>
+
+   </Grid>
+   
+   
+    // <div className='Nav-bar'>
+    //     <div className='title'>
+    //         <h1>Random Quote Generator</h1>
+    //     </div>
+
+    // <div className='nav-element'>
+    // <a href='https://github.com/mirthepeer/Random_QuoteAPI'>
+    //     <div className='view-link'>
+    //         <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVrnjjT5fU58JiJk9w4L6VXCNtVxajn3Ygt3mWSGodgQ&s'></img></div>
+    //         <div><p className='view-code'>  View Code </p></div>
+    //     </div>
     
         
-       </a>
-    </div>
+    //    </a>
+    // </div>
 
-    </div>
+    // </div>
   )
 }
 
--- src/components/QuotCard.js
+++ src/components/QuotCard.js
@@ -1,5 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import './QuotCard.css'
+import Button from '@material-ui/core/Button'
+import 'fontsource-roboto'
+import Topography from '@material-ui/core/Typography'
+import { Container } from '@material-ui/core'
+
+import Paper from '@material-ui/core/Paper'
+import {makeStyles} from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia  from '@material-ui/core/CardMedia'
+import CardActions  from '@material-ui/core/CardActions'
+import CardActioArea  from '@material-ui/core/CardActionArea'
+import Grid from '@material-ui/core/Grid';
+import { blue } from '@material-ui/core/colors'
+
+
+
+
 
 function QuotCard() {
   
@@ -31,20 +49,52 @@ function QuotCard() {
 
   }, [button])
 
-  
+  const useStyles = makeStyles({
+    root: {
+      minHeight:40, textAlign:'center'
+    },
+  })
+  const classes = useStyles()
   return (
     
-    <div className='card'>
-        <div className='card-container'>
-        <div className='card-title'><h1>{quote.author}</h1></div>
-        <div className='card-body'><p>{quote.content}</p></div>
-        <div className='button'>
-            <button className='' onClick={refresh}>New quote</button>
-            </div>
+    <div className={classes.root}>
+         
+    <Grid
+       container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '70vh' }}
+  >      
+   <Card style={{ margin:20, textAllign: 'center', background:'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,56,255,0.5938084706538865) 100%, rgba(0,172,227,0.4481502073485645) 100%)', color:'white'  }}>
+        <CardActioArea>
+        <CardContent style={{height:200, width:600, padding:20} }>
+          <Topography style={{borderRadius:10, borderBlockColor: 'black', background:'white', color:'black'}}>{quote.author}</Topography>
+          <Topography>{quote.content}</Topography>
+        </CardContent>
         
-        </div>
+        
+        <Button 
+        variant='contained'
+        color="success"
+        onClick={refresh}
+        size='large'
+        style={{marginBottom:20}}
+        
+    
+    >New Quote</Button>
+
+        </CardActioArea>
+      
+
+        </Card>
+  
+  </Grid>
+  
+  </div>
        
-    </div>
+    
   )
 }
 
